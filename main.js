2688c4495ee6d50cf2321bcf75c235c7e28d5ac6
MR_mathivanan <166470942+mathivanana123@users.noreply.github.com>
2025-04-18 22:03:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function OpenSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+
+ function CloseSidebar(){
+
+    const clsiSdebar = document.querySelector(".sidebar");
+    clsiSdebar.style.display = "none";
+ }
